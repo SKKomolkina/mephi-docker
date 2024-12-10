92f6c8f25f81bc11b4c1e004194dc7c467b8ef48
Skkomolkina <skomolkina@gmail.com>
2024-12-10 20:13:52 +0300

    add jest

---
+++ app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('Проверка наличия разметки', () => {
+    it('should return an text markup', async () => {
+        const res = await request(app).get('/');
+            expect(res.status).toBe(200);
+            expect(res.text).toContain('<p>💜</p>');
+    });
+});
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const {PORT = 3001} = process.env;
+const {PORT = 3000} = process.env;
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, () => console.log(`Homework for mephi conveer listening on port ${PORT}`));
+
+module.exports = app;
